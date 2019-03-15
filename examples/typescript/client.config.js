ad70210a91bddb254860daef100bc6d1584f42fd
d0p3t <d0p3t@users.noreply.github.com>
2019-03-15 15:12:56 +0100

    added typescript example project

---
+++ examples/typescript/client.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/dist/',
+  },
+};
