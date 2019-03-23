c80879e6a15db03b40a8327bb3b53cb241526d31
Daave <daave@aaathats3as.com>
2019-03-23 17:39:21 +0000

    World enums

---
+++ src/enums/world/Weather.ts
@@ -0,0 +1,21 @@
+/**
+ * List of weather types. Used for manipulating weather.
+ */
+export enum Weather {
+  Unknown = -1,
+  ExtraSunny,
+  Clear,
+  Clouds,
+  Smog,
+  Foggy,
+  Overcast,
+  Raining,
+  ThunderStorm,
+  Clearing,
+  Neutral,
+  Snowing,
+  Blizzard,
+  Snowlight,
+  Christmas,
+  Halloween,
+}
---
+++ src/enums/world/index.ts
@@ -0,0 +1,7 @@
+export { ZoneID } from './ZoneID';
+export { Weather } from './Weather';
+export { WeatherTypeHash } from './WeatherTypeHash';
+export { CloudHat } from './CloudHat';
+export { IntersectOptions } from './IntersectOptions';
+export { MarkerType } from './MarkerType';
+export { ExplosionType } from './ExplosionType';
