9d4b8c1eca5769fcedeea61d5348a8afbef86e4c
Daave <daave@aaathats3as.com>
2019-05-04 20:28:37 +0100

    Cleanup

--- src/ui/Text.ts
+++ src/ui/Text.ts
@@ -10,24 +10,17 @@ export class Text extends IElement {
   public color: Color;
   public font: number;
   public centered: boolean;
-  constructor(caption, pos, scale, color, font, centered) {
+  constructor(caption, pos, scale, color = Color.Black, font = 0, centered = false) {
     super();
     this.caption = caption;
     this.pos = pos;
     this.scale = scale;
-    this.color = color || Color.Black;
-    this.font = font || 0;
-    this.centered = centered || false;
+    this.color = color;
+    this.font = font;
+    this.centered = centered;
   }
 
-  public Draw(caption, pos?, scale?, color?: Color, font?, centered?): void {
-    if (caption && !pos && !scale && !color && !font && !centered) {
-      pos = new Point(this.pos.X + caption.Width, this.pos.Y + caption.Height);
-      scale = this.scale;
-      color = this.color;
-      font = this.font;
-      centered = this.centered;
-    }
+  public Draw(caption, pos = new Point(this.pos.X + caption.Width, this.pos.Y + caption.Height), scale = this.scale, color: Color = this.color, font = this.font, centered = this.centered): void {
     const x = pos.X / Screen.ScaledWidth;
     const y = pos.Y / Screen.Height;
 
