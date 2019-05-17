d39341e89b64b38de0aad22982c69b67d557c63b
d0p3t <d0p3t@users.noreply.github.com>
2019-05-17 16:18:49 +0200

    Update examples due to new tslint config

--- examples/javascript/src/index.js
+++ examples/javascript/src/index.js
@@ -4,8 +4,8 @@ RegisterCommand(
   'adder',
   async (source, args) => {
     const playerCoords = Cfx.Game.PlayerPed.Position;
-    const vehicle = await Cfx.World.CreateVehicle(new Cfx.Model('adder'), playerCoords, 4);
-    Cfx.Game.PlayerPed.SetIntoVehicle(vehicle, Cfx.VehicleSeat.Driver);
+    const vehicle = await Cfx.World.createVehicle(new Cfx.Model('adder'), playerCoords, 4);
+    Cfx.Game.PlayerPed.setIntoVehicle(vehicle, Cfx.VehicleSeat.Driver);
   },
   false,
 );
