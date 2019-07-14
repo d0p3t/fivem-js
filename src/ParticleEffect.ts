d3474a703ffe685f6357edb123a419e91b6c97e6
d0p3t <d0p3t@users.noreply.github.com>
2019-07-14 17:07:03 +0200

    ParticleEffect ParticleEffectAsset comments for docs

--- src/ParticleEffect.ts
+++ src/ParticleEffect.ts
@@ -3,6 +3,9 @@ import { IInvertAxis } from './enums';
 import { Color, Vector3 } from './utils';
 
 // TODO: Lots of Matrix stuff through memory access
+/**
+ * UNFINISHED! Class to manage particle effects.
+ */
 export abstract class ParticleEffect {
   protected readonly asset: ParticleEffectAsset;
   protected readonly effectName: string;
@@ -14,22 +17,37 @@ export abstract class ParticleEffect {
   protected invertAxis: IInvertAxis;
   private handle: number;
 
+  /**
+   * Creates a particle effect.
+   *
+   * @param asset Particle effect asset.
+   * @param effectName Name of effect.
+   */
   constructor(asset: ParticleEffectAsset, effectName: string) {
     this.handle = -1;
     this.asset = asset;
     this.effectName = effectName;
   }
 
+  /**
+   * Get the particle effect handle.
+   */
   public get Handle(): number {
     return this.handle;
   }
 
+  /**
+   * Get whether particle effect is currently active.
+   */
   public get IsActive(): boolean {
     return this.Handle !== -1 && !!DoesParticleFxLoopedExist(this.Handle);
   }
 
   public abstract start(): boolean;
 
+  /**
+   * Stop a particle effect.
+   */
   public stop(): void {
     if (this.IsActive) {
       RemoveParticleFx(this.Handle, false);
@@ -37,10 +55,16 @@ export abstract class ParticleEffect {
     this.handle = -1;
   }
 
+  /**
+   * Get the rotation of the particle effect.
+   */
   public get Rotation(): Vector3 {
     return this.rotation;
   }
 
+  /**
+   * Set the rotation of the particle effect.
+   */
   public set Rotation(rotation: Vector3) {
     this.rotation = rotation;
     if (this.IsActive) {
@@ -57,19 +81,31 @@ export abstract class ParticleEffect {
     }
   }
 
+  /**
+   * Get the range of the particle effect.
+   */
   public get Range(): number {
     return this.range;
   }
 
+  /**
+   * Set the range of the particle effect.
+   */
   public set Range(range: number) {
     this.range = range;
     SetParticleFxLoopedRange(this.Handle, range);
   }
 
+  /**
+   * Get the invert axis flag of the particle effect.
+   */
   public get InvertAxis(): IInvertAxis {
     return this.invertAxis;
   }
 
+  /**
+   * Set the inverted axis of the particle effect.
+   */
   public set InvertAxis(invertAxis: IInvertAxis) {
     this.invertAxis = invertAxis;
     if (this.IsActive) {
@@ -78,20 +114,35 @@ export abstract class ParticleEffect {
     }
   }
 
+  /**
+   * Set a paramaeter of a particle effect.
+   *
+   * @param parameterName Name of parameter.
+   * @param value Value of parameter.
+   */
   public setParameter(parameterName: string, value: number): void {
     if (this.IsActive) {
       SetParticleFxLoopedEvolution(this.Handle, parameterName, value, false);
     }
   }
 
+  /**
+   * Get the name of the particle effect asset. Same as ParticleEffect.AssetName.
+   */
   public get AssetName(): string {
     return this.asset.AssetName;
   }
 
+  /**
+   * Get the name of the particle effect.
+   */
   public get EffectName(): string {
     return this.effectName;
   }
 
+  /**
+   * Return the particle effect as string. `AssetName`\\`EffectName`.
+   */
   public toString(): string {
     return `${this.AssetName}\\${this.EffectName}`;
   }
