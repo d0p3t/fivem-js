17391329ec31fc3423a710ee2ae57a4403958e96
d0p3t <d0p3t@users.noreply.github.com>
2019-10-03 21:37:55 +0200

    'Real' fadeIn & fadeOut

--- src/ui/Fading.ts
+++ src/ui/Fading.ts
@@ -15,11 +15,31 @@ export abstract class Fading {
     return !!IsScreenFadingOut();
   }
 
-  public static fadeIn(duration: number): void {
-    DoScreenFadeIn(duration);
+  public static fadeIn(duration: number): Promise<void> {
+    return new Promise((resolve) => {
+      DoScreenFadeIn(duration);
+
+      const interval = setInterval(() => {
+        if (this.IsFadedIn) {
+          clearInterval(interval);
+          resolve();
+        }
+        // tslint:disable-next-line: align
+      }, 0);
+    });
   }
 
-  public static fadeOut(duration: number): void {
-    DoScreenFadeOut(duration);
+  public static fadeOut(duration: number): Promise<void> {
+    return new Promise((resolve) => {
+      DoScreenFadeOut(duration);
+
+      const interval = setInterval(() => {
+        if (this.IsFadedOut) {
+          clearInterval(interval);
+          resolve();
+        }
+        // tslint:disable-next-line: align
+      }, 0);
+    });
   }
 }
