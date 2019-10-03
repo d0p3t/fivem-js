a690462f78aeb66240a0bf7ca576e060d0be8cf5
d0p3t <d0p3t@users.noreply.github.com>
2019-10-03 21:42:01 +0200

    Add comments to Fading class for docs

--- src/ui/Fading.ts
+++ src/ui/Fading.ts
@@ -1,20 +1,48 @@
+/**
+ * Static class for screen fading
+ */
 export abstract class Fading {
+  /**
+   * Gets whether the screen is faded in
+   *
+   * @returns True or false
+   */
   public static get IsFadedIn(): boolean {
     return !!IsScreenFadedIn();
   }
 
+  /**
+   * Gets whether the screen is faded out
+   *
+   * @returns True or false
+   */
   public static get IsFadedOut(): boolean {
     return !!IsScreenFadedOut();
   }
 
+  /**
+   * Gets whether the screen is currently fading in
+   *
+   * @returns True or false
+   */
   public static get IsFadingIn(): boolean {
     return !!IsScreenFadingIn();
   }
 
+  /**
+   * Gets whether the screen is currently fading out
+   *
+   * @returns True or false
+   */
   public static get IsFadingOut(): boolean {
     return !!IsScreenFadingOut();
   }
 
+  /**
+   * Fade in the screen for a certain duration.
+   *
+   * @param duration Time to fade in
+   */
   public static fadeIn(duration: number): Promise<void> {
     return new Promise((resolve) => {
       DoScreenFadeIn(duration);
@@ -29,6 +57,11 @@ export abstract class Fading {
     });
   }
 
+  /**
+   * Fade out the screen for a certain duration.
+   *
+   * @param duration Time to fade out
+   */
   public static fadeOut(duration: number): Promise<void> {
     return new Promise((resolve) => {
       DoScreenFadeOut(duration);
