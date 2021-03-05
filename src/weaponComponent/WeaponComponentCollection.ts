c8d55ccfd4846cdc1914bccc2bbd240fd8ac1c52
imckl <imckl@outlook.com>
2021-03-06 01:12:36 +0800

    Add WeaponComponentCollection

---
+++ src/weaponComponent/WeaponComponentCollection.ts
@@ -0,0 +1,174 @@
+import { Ped } from '../models';
+import { Weapon } from '../weapon/Weapon';
+import { WeaponComponentHash } from './WeaponComponentHash';
+import { WeaponComponent } from './WeaponComponent';
+import { InvalidWeaponComponent } from './InvalidWeaponComponent';
+import { WeaponComponentHashesByWeaponHash } from './WeaponComponentHashesByWeaponHash';
+import { ComponentAttachmentPoint } from './ComponentAttachmentPoint';
+
+
+export class WeaponComponentCollection implements Iterable<WeaponComponent> {
+  private readonly owner: Ped;
+  private readonly weapon: Weapon;
+
+  private readonly components = new Map<WeaponComponentHash, WeaponComponent>();
+
+  private readonly componentHashes: WeaponComponentHash[];
+
+  private readonly invalidComponent = new InvalidWeaponComponent();
+
+  public constructor(
+    owner: Ped,
+    weapon: Weapon,
+  ) {
+    this.owner = owner;
+    this.weapon = weapon;
+    this.componentHashes = WeaponComponentHashesByWeaponHash[this.weapon.Hash];
+  }
+
+  [Symbol.iterator](): Iterator<WeaponComponent> {
+    let pointer = 0;
+    const components = this.components;
+
+    return {
+      next(): IteratorResult<WeaponComponent> {
+        if (pointer < components.size) {
+          return { done: false, value: components[pointer++] };
+        } else {
+          return { done: true, value: null };
+        }
+      },
+    };
+  }
+
+  public get(componentHash: WeaponComponentHash): WeaponComponent {
+    if (this.componentHashes.some(x => x === componentHash)) {
+      let component = this.components.get(componentHash);
+
+      if (!component) {
+        component = new WeaponComponent(this.owner, this.weapon, componentHash);
+        this.components.set(componentHash, component);
+      }
+
+      return component;
+    }
+
+    return this.invalidComponent;
+  }
+
+  public get Count(): number {
+    return this.components.size;
+  }
+
+  public GetClipComponent(index: number): WeaponComponent {
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if ([ComponentAttachmentPoint.Clip, ComponentAttachmentPoint.Clip2].some(x => x === attachmentPoint)) {
+        if (index-- === 0) {
+          return component;
+        }
+      }
+    }
+
+    return this.invalidComponent;
+  }
+
+  public get ClipVariationsCount(): number {
+    let count = 0;
+
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if ([ComponentAttachmentPoint.Clip, ComponentAttachmentPoint.Clip2].some(x => x === attachmentPoint)) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  public GetScopeComponent(index: number): WeaponComponent {
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if ([ComponentAttachmentPoint.Scope, ComponentAttachmentPoint.Scope2].some(x => x === attachmentPoint)) {
+        if (index-- === 0) {
+          return component;
+        }
+      }
+    }
+
+    return this.invalidComponent;
+  }
+
+  public get ScopeVariationsCount(): number {
+    let count = 0;
+
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if ([ComponentAttachmentPoint.Scope, ComponentAttachmentPoint.Scope2].some(x => x === attachmentPoint)) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  public GetSuppressorComponent(): WeaponComponent {
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if ([ComponentAttachmentPoint.Supp, ComponentAttachmentPoint.Supp2].some(x => x === attachmentPoint)) {
+        return component;
+      }
+    }
+
+    return this.invalidComponent;
+  }
+
+  public GetFlashLightComponent(): WeaponComponent {
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if ([ComponentAttachmentPoint.FlashLaser, ComponentAttachmentPoint.FlashLaser2].some(x => x === attachmentPoint)) {
+        return component;
+      }
+    }
+
+    return this.invalidComponent;
+  }
+
+  public GetLuxuryFinishComponent(): WeaponComponent {
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if (attachmentPoint === ComponentAttachmentPoint.GunRoot) {
+        return component;
+      }
+    }
+
+    return this.invalidComponent;
+  }
+
+  public GetMk2CamoComponent(index: number): WeaponComponent {
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if (attachmentPoint === ComponentAttachmentPoint.GunRoot) {
+        if (index-- === 0) {
+          return component;
+        }
+      }
+    }
+
+    return this.invalidComponent;
+  }
+
+  public GetMk2BarrelComponent(index: number): WeaponComponent {
+    for (const [, component] of this.components) {
+      const attachmentPoint = component.AttachmentPoint;
+      if (attachmentPoint === ComponentAttachmentPoint.Barrel) {
+        if (index-- === 0) {
+          return component;
+        }
+      }
+    }
+
+    return this.invalidComponent;
+  }
+
+}
