621f8d8f3fd5c76cbb2c4a2ff4815cf32a17dcdc
imckl <imckl@outlook.com>
2021-03-06 01:12:25 +0800

    Add InvalidWeaponComponent

---
+++ src/weaponComponent/InvalidWeaponComponent.ts
@@ -0,0 +1,32 @@
+import { WeaponComponent } from './WeaponComponent';
+import { WeaponComponentHash } from './WeaponComponentHash';
+import { WeaponHash } from '../hashes';
+import { ComponentAttachmentPoint } from './ComponentAttachmentPoint';
+import { Game } from '../Game';
+
+export class InvalidWeaponComponent extends WeaponComponent{
+  constructor() {
+    super(null, null, WeaponComponentHash.Invalid);
+  }
+
+  public get Active(): boolean {
+    return false;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public set Active(value: boolean) {
+  }
+
+  public get DisplayName(): string {
+    return 'WCT_INVALID';
+  }
+
+  public get LocalizedName(): string {
+    return Game.GetGXTEntry(this.DisplayName);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public static GetAttachmentPoint(hash: WeaponHash, componentHash: WeaponComponentHash): ComponentAttachmentPoint{
+    return ComponentAttachmentPoint.Invalid;
+  }
+}
