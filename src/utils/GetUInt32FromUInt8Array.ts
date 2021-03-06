20ec9c1fed212502e37cbff835da84df03805ee3
imckl <imckl@outlook.com>
2021-03-06 21:45:04 +0800

    Add: getUInt32FromUint8Array - get uint32 from uint8 array

---
+++ src/utils/GetUInt32FromUInt8Array.ts
@@ -0,0 +1,10 @@
+/**
+ * get uint32 from uint8 array
+ *
+ * @param buffer - Uint8Array
+ * @param start - The beginning of the specified portion of the array
+ * @param end - The end of the specified portion of the array
+ */
+export const getUInt32FromUint8Array =
+  (buffer: Uint8Array, start: number, end: number): number =>
+    new Uint32Array(buffer.slice(start, end).buffer)[0];
