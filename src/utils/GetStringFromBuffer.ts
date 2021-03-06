d95a038d050af6e92b796e0f14695a12b3a4b466
imckl <imckl@outlook.com>
2021-03-06 21:44:51 +0800

    Add: getStringFromUInt8Array - get string from uint8 array

---
+++ src/utils/GetStringFromBuffer.ts
@@ -0,0 +1,13 @@
+/**
+ * get string from uint8 array
+ *
+ * @param buffer - Uint8Array
+ * @param start - The beginning of the specified portion of the array
+ * @param end - The end of the specified portion of the array
+ */
+export const getStringFromUInt8Array =
+  (buffer: Uint8Array, start: number, end: number): string =>
+    String
+      .fromCharCode(...buffer.slice(start, end))
+      // eslint-disable-next-line no-control-regex
+      .replace(/\u0000/g, '');
