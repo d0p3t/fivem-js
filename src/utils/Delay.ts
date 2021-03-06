ad953e623135ba4dbfd1534ab1e5bf4c0e6e8f0c
imckl <imckl@outlook.com>
2021-03-06 22:59:40 +0800

    Update: Remove Delay - in favor of Wait

--- src/utils/Delay.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Port of BaseScript.Delay
- *
- * @param ms
- * @constructor
- */
-export const Delay: (ms: number) => Promise<void> =
-  (ms: number) => new Promise(res => setTimeout(res, ms));
