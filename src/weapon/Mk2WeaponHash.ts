77f402c8f1d50a1632ea627a4333abd2eef3abef
imckl <imckl@outlook.com>
2021-03-07 13:56:35 +0800

    Add: Mk2WeaponHash

---
+++ src/weapon/Mk2WeaponHash.ts
@@ -0,0 +1,29 @@
+/**
+ * Mk2 weapon hash
+ *
+ */
+export enum Mk2WeaponHash {
+  // handguns
+  PistolMk2 = 0xBFE256D4,
+  SNSPistolMk2 = 0x88374054,
+  RevolverMk2 = 0xCB96392F,
+
+  // smg
+  SMGMk2 = 0x78A97CD0,
+
+  // shotgun
+  PumpShotgunMk2 = 0x555AF99A,
+
+  // assault_rifles
+  AssaultRifleMk2 = 0x394F415C,
+  CarbineRifleMk2 = 0xFAD1F1C9,
+  SpecialCarbineMk2 = 0x969C3D67,
+  BullpupRifleMk2 = 0x84D6FAFD,
+
+  // machine_guns
+  CombatMGMk2 = 0xDBBD7280,
+
+  // sniper_rifles
+  HeavySniperMk2 = 0xA914799,
+  MarksmanRifleMk2 = 0x6A6C02E0,
+}
