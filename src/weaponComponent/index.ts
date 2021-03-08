829fd19456e468c4da06e24069df298260872faa
imckl <imckl@outlook.com>
2021-03-08 17:02:33 +0800

    Update: expose regular weapon, weaponComponent files to root

---
+++ src/weaponComponent/index.ts
@@ -0,0 +1,5 @@
+export { DlcWeaponComponentData } from './DlcWeaponComponentData';
+export { WeaponComponentHudStats } from './WeaponComponentHudStats';
+export { ComponentAttachmentPoint } from './ComponentAttachmentPoint';
+export { WeaponComponentHash } from './WeaponComponentHash';
+export { InvalidWeaponComponent } from './InvalidWeaponComponent';
