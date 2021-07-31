7c7b50020257e1b869a0b86d37a8b679b484682f
William Desgardin <williamdesgardin@gmail.com>
2021-07-31 20:01:35 +0200

    feat: add GamerTag and GamerTagComponents

---
+++ src/GamerTag.ts
@@ -0,0 +1,50 @@
+import { HudColor } from './enums';
+import { GamerTagComponent } from './enums/GamerTagComponent';
+
+export class GamerTag {
+  protected handle: number;
+
+  constructor(handle: number) {
+    this.handle = handle;
+  }
+
+  public get Handle(): number {
+    return this.handle;
+  }
+
+  public get IsActive(): boolean {
+    return !!IsMpGamerTagActive(this.handle);
+  }
+
+  public get IsFree(): boolean {
+    return !!IsMpGamerTagFree(this.handle);
+  }
+
+  public set Name(value: string) {
+    SetMpGamerTagName(this.handle, value);
+  }
+
+  public set WantedLevel(level: number) {
+    SetMpGamerTagWantedLevel(this.handle, level);
+  }
+
+  public setHealthBarColour(color: HudColor) {
+    SetMpGamerTagHealthBarColour(this.handle, color);
+  }
+
+  public setComponentAlpha(component: GamerTagComponent, alpha: number): void {
+    SetMpGamerTagAlpha(this.handle, component, alpha);
+  }
+
+  public setComponentColour(component: GamerTagComponent, hudColorIndex: HudColor): void {
+    SetMpGamerTagColour(this.handle, component, hudColorIndex);
+  }
+
+  public setComponentVisibility(component: GamerTagComponent, visible: boolean): void {
+    SetMpGamerTagVisibility(this.handle, component, visible);
+  }
+
+  public delete(): void {
+    RemoveMpGamerTag(this.handle);
+  }
+}
